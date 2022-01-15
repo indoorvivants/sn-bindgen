0b000f08c7851cf88a196e171e6768c80be3701b
Anton Sviridov <keynmol@gmail.com>
2022-01-15 08:31:19 +0000

    Undo buildscript changes

--- build/setup_unix.sh
+++ build/setup_unix.sh
@@ -5,13 +5,9 @@ PLATFORM="$(uname)"
 if [ $PLATFORM == "Darwin" ]; then
   echo "It's a Mac"
   brew install llvm
-  java -version
-  curl -Lo /usr/local/bin/sbt https://raw.githubusercontent.com/sbt/sbt/v1.6.1/sbt
 else
   echo "It's a linux"
-  apt update
-  apt install clang llvm llvm-dev libclang-dev
-  java -version
-  curl -Lo /usr/local/bin/sbt https://raw.githubusercontent.com/sbt/sbt/v1.6.1/sbt
+  sudo apt update
+  sudo apt install clang llvm llvm-dev libclang-dev
 fi
 
