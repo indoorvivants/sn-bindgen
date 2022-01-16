2c637f3c6e04a61f1fba315b75571dbd0b70213c
Anton Sviridov <keynmol@gmail.com>
2022-01-16 16:03:23 +0000

    Install LLVM 13 on Ubuntu (#25)

--- build/setup_unix.sh
+++ build/setup_unix.sh
@@ -8,6 +8,10 @@ if [ $PLATFORM == "Darwin" ]; then
 else
   echo "It's a linux"
   sudo apt update
-  sudo apt install clang llvm llvm-dev libclang-dev
+  sudo apt install lsb-release wget software-properties-common
+  wget https://apt.llvm.org/llvm.sh
+  chmod +x llvm.sh
+  sudo ./llvm.sh 13
+  sudo apt install libclang-13-dev
 fi
 
