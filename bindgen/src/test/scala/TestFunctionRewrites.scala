4caa802bf0e9c5f348b0d4758b15c53df5c8d27b
Anton Sviridov <keynmol@gmail.com>
2022-01-22 17:28:36 +0000

    Generate pointer versions of struct passing functions (#35)

--- bindgen/src/test/scala/TestFunctionRewrites.scala
+++ bindgen/src/test/scala/TestFunctionRewrites.scala
@@ -26,7 +26,6 @@ class TestFunctionRewrites:
     zone {
       val t1 = FunctionRewriteStruct(25, 35.0)
       val t2 = FunctionRewriteStruct(15, 40.0)
-      val i = 100
 
       val result = rewrite_bad_func(!t1, !t2)
 
@@ -42,5 +41,23 @@ class TestFunctionRewrites:
 
       assertEquals(25 - 15, rewrite_better_func(!t1, !t2))
       assertEquals(15 - 25, rewrite_better_func(!t2, !t1))
+    }
+
+  @Test def test_pointer_versions(): Unit =
+    zone {
+      val t1 = FunctionRewriteStruct(25, 35.0)
+      val t2 = FunctionRewriteStruct(15, 40.0)
+      val resultPtr = FunctionRewriteStruct()
+      {
+        // this method shouldn't require a Zone!
+        given Zone = ???
+        rewrite_bad_func(t1, t2)(resultPtr)
+
+        assertEquals(35.0 * 40.0, (!resultPtr).b, 0.01f)
+      }
+
+      val result = rewrite_bad_func(t1, t2)
+
+      assertEquals(35.0 * 40.0, result.b, 0.01f)
     }
 end TestFunctionRewrites
