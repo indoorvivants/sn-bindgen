e71e79ba331fdc90fa05895e704a59774c7a1249
Anton Sviridov <keynmol@gmail.com>
2022-02-28 18:00:12 +0000

    Fix on windows

--- bindgen/src/test/resources/scala-native/conflicting_names.h
+++ bindgen/src/test/resources/scala-native/conflicting_names.h
@@ -1,6 +1,6 @@
 
 typedef int EVP_MD_CTX;
-typedef int size_t;
+typedef int bla_bla;
 
-int (*EVP_MD_CTX_update_fn(EVP_MD_CTX *ctx))(EVP_MD_CTX *ctx,
-                                             const void *data, size_t count);
+int (*EVP_MD_CTX_update_fn(EVP_MD_CTX *ctx))(EVP_MD_CTX *ctx, const void *data,
+                                             bla_bla count);
