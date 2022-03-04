20783bcd1725b60c0e8629c254b2cb98f2eeceb8
Anton Sviridov <keynmol@gmail.com>
2022-03-04 11:21:19 +0000

    Fix CNAME in docs

--- docs/src/main/scala/Docs.scala
+++ docs/src/main/scala/Docs.scala
@@ -2,6 +2,10 @@ package bindgen.docs
 
 import subatomic.builders.librarysite.*
 import subatomic.builders.*
+import subatomic.*
+import java.time.LocalDate
+
+def currentYear = LocalDate.now().getYear()
 
 object Docs extends LibrarySite.App:
   def config =
@@ -9,5 +13,13 @@ object Docs extends LibrarySite.App:
       name = "SN bindgen",
       contentRoot = os.pwd / "pages",
       highlighting =
-        SyntaxHighlighting.HighlightJS.default.copy(theme = "monokai")
+        SyntaxHighlighting.HighlightJS.default.copy(theme = "monokai"),
+      tagline = Some("Scala 3 Native binding generator to C libraries"),
+      assetsRoot = Some(os.pwd / "assets"),
+      copyright = Some(s"© 2021-$currentYear Anton Sviridov"),
+      githubUrl = Some("https://github.com/indoorvivants/sn-bindgen")
     )
+  override def extra(site: Site[LibrarySite.Doc]) =
+    site
+      .addCopyOf(SiteRoot / "CNAME", os.pwd / "assets" / "CNAME")
+end Docs
