23ce01c092a28d1ab7f368f449ab4b758e071414
Anton Sviridov <keynmol@gmail.com>
2022-03-06 14:56:38 +0000

    Render only what is needed

--- bindgen/src/main/scala/render/binding.scala
+++ bindgen/src/main/scala/render/binding.scala
@@ -19,21 +19,43 @@ def binding(
     |import scalanative.libc.*
     """.stripMargin.trim)
   scalaOutput.emptyLines(2)
-  scalaOutput.appendLine("object predef:")
-  nest {
-    val predef = """
-    |abstract class CEnum[T](using eq: T =:= Int):
-    |  given Tag[T] = Tag.Int.asInstanceOf[Tag[T]]
-    |  extension (t: T) def int: CInt = eq.apply(t)
-    |
-    |abstract class CEnumU[T](using eq: T =:= UInt):
-    |  given Tag[T] = Tag.UInt.asInstanceOf[Tag[T]]
-    |  extension (t: T)
-    |   def int: CInt = eq.apply(t).toInt
-    |   def uint: CUnsignedInt = eq.apply(t)
-    """.stripMargin.linesIterator
-    predef.foreach(to(scalaOutput))
-  }
+  val hasUnsignedEnums =
+    binding.enums.exists(_.intType.exists(_.sign == SignType.Unsigned))
+
+  val hasSignedEnums =
+    binding.enums.exists(en =>
+      en.intType.exists(_.sign == SignType.Signed) || en.intType.isEmpty
+    )
+
+  val hasAnyEnums = binding.enums.nonEmpty
+  val hasAliases = binding.aliases.nonEmpty
+  val hasUnions = binding.unions.nonEmpty
+  val hasStructs = binding.structs.nonEmpty
+  val hasAnyTypes = hasAnyEnums || hasAliases || hasUnions || hasStructs
+
+  if (hasAnyEnums) then
+    scalaOutput.appendLine("object predef:")
+    nest {
+
+      val predefSigned = s"""
+      |trait CEnum[T](using eq: T =:= Int):
+      |  given Tag[T] = Tag.Int.asInstanceOf[Tag[T]]
+      |  extension (t: T) def int: CInt = eq.apply(t)
+     """.stripMargin.linesIterator
+
+      val predefUnsigned = s"""
+      |trait CEnumU[T](using eq: T =:= UInt):
+      |  given Tag[T] = Tag.UInt.asInstanceOf[Tag[T]]
+      |  extension (t: T)
+      |   def int: CInt = eq.apply(t).toInt
+      |   def uint: CUnsignedInt = eq.apply(t)
+      """.stripMargin.linesIterator
+
+      if (hasSignedEnums) then predefSigned.foreach(to(scalaOutput))
+      if (hasUnsignedEnums) then predefUnsigned.foreach(to(scalaOutput))
+
+    }
+  end if
 
   type Scope = "predef" | "aliases" | "enumerations" | "structs" | "functions"
   def define(s: Scope) = s"object $s"
@@ -57,7 +79,8 @@ def binding(
     throw exc
   end commentException
 
-  scalaOutput.appendLine("object types:")
+  if hasAnyTypes then scalaOutput.appendLine("object types:")
+
   def renderAll[A <: (Def | GeneratedFunction)](
       defs: Seq[A],
       out: LineBuilder,
@@ -82,14 +105,15 @@ def binding(
     }
 
   nest {
-    to(scalaOutput)("import predef.*")
-    renderAll(
-      binding.enums.toList
-        .sortBy(_.name)
-        .filter(_.name.isDefined),
-      scalaOutput,
-      enumeration
-    )
+    if hasAnyEnums then
+      to(scalaOutput)("import predef.*")
+      renderAll(
+        binding.enums.toList
+          .sortBy(_.name)
+          .filter(_.name.isDefined),
+        scalaOutput,
+        enumeration
+      )
     renderAll(binding.aliases.toList.sortBy(_.name), scalaOutput, alias)
     renderAll(binding.structs.toList.sortBy(_.name), scalaOutput, struct)
     renderAll(binding.unions.toList.sortBy(_.name), scalaOutput, union)
@@ -110,31 +134,43 @@ def binding(
       f
   }
 
+  val hasExternFunctions = scalaExternFunctions.nonEmpty
+  val hasRegularFunctions = scalaRegularFunctions.nonEmpty
+
   if binding.functions.nonEmpty then
-    summon[Config].linkName.foreach { l =>
-      scalaOutput.append(s"""@link("$l")""")
-    }
-    scalaOutput.append(
-      s"\n@extern\nprivate[$packageName] object extern_functions: \n"
-    )
-    nest {
-      to(scalaOutput)("import types.*\n")
-      renderAll(
-        scalaExternFunctions.toList.sortBy(_.name),
-        scalaOutput,
-        function
+    if hasExternFunctions then
+      summon[Config].linkName.foreach { l =>
+        scalaOutput.append(s"""@link("$l")""")
+      }
+      scalaOutput.appendLine(
+        s"\n@extern\nprivate[$packageName] object extern_functions:"
       )
-    }
-    scalaOutput.append(s"\nobject functions: \n")
-    nest {
-      to(scalaOutput)("import types.*, extern_functions.*\n")
-      to(scalaOutput)("export extern_functions.*\n")
-      renderAll(
-        scalaRegularFunctions.toList.sortBy(_.name),
-        scalaOutput,
-        function
-      )
-    }
+      nest {
+        if (hasAnyTypes) then to(scalaOutput)("import types.*")
+        renderAll(
+          scalaExternFunctions.toList.sortBy(_.name),
+          scalaOutput,
+          function
+        )
+      }
+    end if
+
+    if hasRegularFunctions || hasExternFunctions then
+      scalaOutput.appendLine(s"\nobject functions:")
+      nest {
+        if hasAnyTypes then to(scalaOutput)("import types.*")
+
+        if hasExternFunctions then
+          to(scalaOutput)("import extern_functions.*")
+          to(scalaOutput)("export extern_functions.*")
+
+        renderAll(
+          scalaRegularFunctions.toList.sortBy(_.name),
+          scalaOutput,
+          function
+        )
+      }
+    end if
 
     val cFunctions = resolvedFunctions.collect {
       case f: GeneratedFunction.CFunction => f
