d3fe1c7f97dea031e9f8f3e4637385d0c9fcd4e2
Anton Sviridov <keynmol@gmail.com>
2022-05-30 18:34:31 +0100

    Try with tail recursive closure computers (#67)

--- bindgen/src/main/scala/analysis/closure.scala
+++ bindgen/src/main/scala/analysis/closure.scala
@@ -1,42 +1,77 @@
 package bindgen
 
 import scala.scalanative.libc.stdio.FILE
+import scala.annotation.tailrec
 
-def definitionClosure(b: Def)(using Config): Set[String] =
-  def ref(ct: CType): Set[String] =
-    import CType.*
-    ct match
-      case Reference(Name.Model(n)) => Set(n)
-      case Struct(fields)           => fields.toSet.flatMap(ref)
-      case Union(fields)            => fields.toSet.flatMap(ref)
-      case Function(retType, params) =>
-        ref(retType) ++ params.toSet.map(_.of).flatMap(ref)
-      case Arr(of, _)  => ref(of)
-      case Pointer(of) => ref(of)
-      case _ =>
-        Set.empty
-    end match
-  end ref
+def referencedNames(typ: CType): Set[String] =
+  import CType.*
 
-  b match
-    case f: Def.Function =>
-      Set(f.name.value) ++
-        ref(f.returnType) ++
-        f.parameters.toSet.flatMap { p =>
-          ref(p.typ) ++ ref(p.originalTyp.typ)
-        }
-    case f: Def.Struct =>
-      Set(f.name.value) ++
-        f.fields
-          .map(_._2)
-          .toSet
-          .flatMap(ref) ++
-        f.anonymous.toSet.flatMap(definitionClosure)
-    case f: Def.Union =>
-      Set(f.name.value) ++ f.fields.map(_._2).toSet.flatMap(ref)
-    case f: Def.Enum  => f.name.toSet.map(_.value)
-    case a: Def.Alias => Set(a.name) ++ ref(a.underlying)
-  end match
+  @tailrec
+  def go(types: List[CType], acc: Set[String]): Set[String] =
+    types match
+      case ct :: rest =>
+        val (next, names) =
+          ct match
+            case Reference(Name.Model(n)) => Nil -> Set(n)
+            case Struct(fields)           => fields -> Set.empty
+            case Union(fields)            => fields -> Set.empty
+            case Function(retType, params) =>
+              (retType :: params.map(_.of)) -> Set.empty
+            case Arr(of, _)  => List(of) -> Set.empty
+            case Pointer(of) => List(of) -> Set.empty
+            case _ =>
+              Nil -> Set.empty
+          end match
+        end val
+
+        go(next ++ rest, acc ++ names)
+      case Nil => acc
+  end go
+
+  go(List(typ), Set.empty)
+end referencedNames
+
+def definitionClosure(_d: Def)(using Config): Set[String] =
+  @tailrec
+  def go(types: List[Def], acc: Set[String]): Set[String] =
+    types match
+      case b :: rest =>
+        val (next, names) =
+          b match
+            case f: Def.Function =>
+              Nil -> {
+                Set(f.name.value) ++
+                  referencedNames(f.returnType) ++
+                  f.parameters.toSet.flatMap { p =>
+                    referencedNames(p.typ) ++ referencedNames(p.originalTyp.typ)
+                  }
+              }
+
+            case f: Def.Struct =>
+              f.anonymous -> {
+                Set(f.name.value) ++
+                  f.fields
+                    .map(_._2)
+                    .toSet
+                    .flatMap(referencedNames)
+              }
+            case f: Def.Union =>
+              Nil -> {
+                Set(f.name.value) ++ f.fields
+                  .map(_._2)
+                  .toSet
+                  .flatMap(referencedNames)
+              }
+            case f: Def.Enum => Nil -> f.name.toSet.map(_.value)
+            case a: Def.Alias =>
+              Nil -> (Set(a.name) ++ referencedNames(a.underlying))
+          end match
+        end val
+
+        go(rest ++ next, names ++ acc)
+      case _ => acc
+
+  go(List(_d), Set.empty)
 end definitionClosure
 
 def computeClosure(named: Map[DefName, BindingDefinition])(using
@@ -44,9 +79,9 @@ def computeClosure(named: Map[DefName, BindingDefinition])(using
 ): Set[String] =
   import scala.collection.mutable
 
+  @tailrec
   def expand(visited: Set[String], result: Set[String]): Set[String] =
     val notVisited = result -- visited
-    // trace(s"Closure computer: visited = $visited, result = $result")
     if notVisited.isEmpty then result
     else
       val grown = notVisited.flatMap { k =>
@@ -57,10 +92,7 @@ def computeClosure(named: Map[DefName, BindingDefinition])(using
             .getOrElse(Set.empty)
         }
       }
-      // trace(s"Closure computer: grown = $grown")
-      if (grown -- result).nonEmpty then
-        expand(visited ++ notVisited, result ++ grown)
-      else result
+      expand(visited ++ notVisited, result ++ grown)
     end if
   end expand
 
