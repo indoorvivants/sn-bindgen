be5c1a676880af0f57157f750e66125b0c3ba360
Anton Sviridov <keynmol@gmail.com>
2022-06-01 12:05:50 +0100

    Cleanup

--- bindgen/src/main/scala/analysis/capture.scala
+++ bindgen/src/main/scala/analysis/capture.scala
@@ -6,7 +6,6 @@ opaque type Memory = (String, () => Unit)
 object Memory:
   extension (f: Memory)
     def deallocate() =
-      System.err.println(s"Deallocating ${f._1}")
       f._2()
 
 type Captured[D] = (D, Config)
@@ -14,7 +13,6 @@ object Captured:
   def unsafe[D: Tag](value: D)(using c: Config): (Ptr[Captured[D]], Memory) =
     import scalanative.runtime.*
 
-    System.err.println(s"Allocating ${value.toString}")
     val rawptr = libc.malloc(sizeof[Captured[D]])
     val mem = fromRawPtr[Captured[D]](rawptr)
     val deallocate: Memory =
@@ -25,8 +23,6 @@ object Captured:
     val originalAddress = Intrinsics.castObjectToRawPtr(tuple)
 
     Intrinsics.storeObject(rawptr, tuple)
-
-    // System.err.println(s"Original address: $originalAddress, ptr: $rawptr")
 
     (mem, deallocate)
   end unsafe
