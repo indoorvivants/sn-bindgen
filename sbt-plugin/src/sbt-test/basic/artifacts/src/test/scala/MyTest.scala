e0bfd074483942d126cfa67c6fe7cf3627bf928d
Anton Sviridov <keynmol@gmail.com>
2022-08-12 14:36:26 +0100

    Scope settings to Compile/Test/Zero (#82)
    This opens the door for having different generated bindings
    in compile and test scopes
    
    It's a step towards removing any custom logic from sn-bindgen's own
    build

---
+++ sbt-plugin/src/sbt-test/basic/artifacts/src/test/scala/MyTest.scala
@@ -0,0 +1,10 @@
+import org.junit.Assert._
+import org.junit.Test
+
+import gentests.functions.*
+
+class MyTest {
+  @Test def superComplicatedTest(): Unit = {
+    assertTrue(hello(42, 0.05f))
+  }
+}
