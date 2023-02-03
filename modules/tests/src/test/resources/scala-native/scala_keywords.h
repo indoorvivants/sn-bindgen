268a9edea3115daea90e930005390c487bbf5389
Anton Sviridov <keynmol@gmail.com>
2023-02-03 15:32:51 +0000

    Reproducers for #153 and other libnotify binding failures

--- modules/tests/src/test/resources/scala-native/scala_keywords.h
+++ modules/tests/src/test/resources/scala-native/scala_keywords.h
@@ -2,10 +2,23 @@ int test_keywords(int def, int class, int final, int object);
 typedef union test_keywords {
   char object;
   long final;
-  void* class;
-  int* def;
+  void *class;
+  int *def;
   int val;
   int var;
   int new;
+  int notify;
+  int wait;
 } UnionKeywords;
 
+typedef struct test_keywords_struct {
+  char object;
+  long final;
+  void *class;
+  int *def;
+  int val;
+  int var;
+  int new;
+  int notify;
+  int wait;
+} StructKeywords;
