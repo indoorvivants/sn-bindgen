a0d609836419ed6999097cf9be73246fe895e33b
Anton Sviridov <keynmol@gmail.com>
2023-02-07 11:55:03 +0000

    Add tests

---
+++ modules/bindgen/src/test/scala/TestRenderingConfig.scala
@@ -0,0 +1,20 @@
+package bindgen
+
+import org.junit.Assert.*
+import org.junit.Test
+
+import scala.scalanative.unsafe.*
+import scala.scalanative.unsigned.*
+import java.nio.file.Files
+
+class TestRenderingConfig:
+  @Test def test_noConstructor() =
+    import RenderingConfig.NoConstructor.apply as nc
+
+    assertEquals(Some("hello"), nc("hello").matches("hello"))
+    assertEquals(None, nc("hello").matches("hello1"))
+
+    assertEquals(None, nc("hello*").matches("1hello1"))
+    assertEquals(Some("hello*"), nc("hello*").matches("hello1"))
+    assertEquals(Some("hello*world*"), nc("hello*world*").matches("hello1asdasdworldasdasd"))
+end TestRenderingConfig
