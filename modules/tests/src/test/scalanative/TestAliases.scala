e2405c468ba6fbc4e52eff8fcb459629f9ca2054
Anton Sviridov <keynmol@gmail.com>
2023-10-16 15:11:28 +0100

    fromPtr/toPtr helper methods for C function pointer aliases (#240)

--- modules/tests/src/test/scalanative/TestAliases.scala
+++ modules/tests/src/test/scalanative/TestAliases.scala
@@ -8,9 +8,10 @@ import scala.scalanative.unsigned.*
 
 class TestAliases:
   import lib_test_aliases.types.*
+  import lib_test_aliases.functions.*
 
   @Test def test_basics() =
-    zone {
+    zone:
       assertEquals(42069, hello_alias(42069).value)
 
       val x: Int = hello_alias(42069).value
@@ -25,5 +26,29 @@ class TestAliases:
 
       assertEquals(t.value, hello_alias(25).value)
       assertEquals(t.value, 25)
-    }
+
+  @Test def test_function_pointers() =
+    zone:
+      val test = TestStruct(10)
+
+      val square = TestFunctionPointer: (struct: Ptr[TestStruct]) =>
+        (!struct)._ne * (!struct)._ne
+
+      // sanity check first
+      assertEquals(500, higher_order_function(5, square, test))
+
+      val ptr: Ptr[Byte] = CFuncPtr.toPtr(square.value)
+      val squareReconstructed =
+        TestFunctionPointer.fromPtr(ptr)
+
+      // now check reconstructed function is still valid
+      assertEquals(500, higher_order_function(5, squareReconstructed, test))
+
+      // test toPtr/fromPtr pair
+      val roundTrip =
+        TestFunctionPointer.fromPtr(square.toPtr)
+
+      // now check reconstructed function is still valid
+      assertEquals(500, higher_order_function(5, roundTrip, test))
+
 end TestAliases
