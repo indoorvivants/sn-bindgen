c618b8df2351d4530d323d0640008dc86ac6ee1d
Anton Sviridov <keynmol@gmail.com>
2023-10-22 12:51:11 +0100

    Machinery in place for CFuncPtr aliases `apply` methods

--- modules/bindgen/src/main/scala/render/alias.scala
+++ modules/bindgen/src/main/scala/render/alias.scala
@@ -3,6 +3,7 @@ package rendering
 
 import bindgen.*
 import scala.scalanative.unsafe.CFuncPtr
+import util.chaining.*
 
 import CType.*
 
@@ -21,9 +22,9 @@ def alias(model: Def.Alias, line: Appender)(using
     case _: Reference | _: Function | Void => false
     case _                                 => true
 
-  val isFunctionPointer = underlyingType match
-    case Pointer(f: Function) => true
-    case _                    => false
+  val underlyingFunction = underlyingType match
+    case Pointer(f: Function) => Some(f)
+    case _                    => None
 
   val modifier = if isOpaque then "opaque " else ""
   renderComment(line, model.meta)
@@ -36,22 +37,48 @@ def alias(model: Def.Alias, line: Appender)(using
       case _ =>
         line(s"given _tag: Tag[${model.name}] = ${scalaTag(underlyingType)}")
 
-    if isFunctionPointer then
+    if underlyingFunction.isDefined then
       line(
         s"inline def fromPtr(ptr: Ptr[Byte]): ${model.name} = CFuncPtr.fromPtr(ptr)"
       )
     end if
 
+    val renderedUnderlyingType = scalaType(underlyingType)
+
     if enableConstructor then
       line(
-        s"inline def apply(inline o: ${scalaType(underlyingType)}): ${model.name} = o"
+        s"inline def apply(inline o: ${renderedUnderlyingType}): ${model.name} = o"
       )
       line(s"extension (v: ${model.name})")
       nest {
-        line(s"inline def value: ${scalaType(underlyingType)} = v")
-        if isFunctionPointer then
+        line(s"inline def value: ${renderedUnderlyingType} = v")
+        underlyingFunction.foreach: func =>
           line(s"inline def toPtr: Ptr[Byte] = CFuncPtr.toPtr(v)")
-        end if
+          val returnType = scalaType(func.returnType)
+          val params = List.newBuilder[String]
+          val arguments = List.newBuilder[String]
+          var counter = 0
+          def newName =
+            s"_$counter".tap(_ => counter += 1)
+
+
+          func.parameters.foreach:
+            case Parameter(name, of) =>
+              val formalName =
+                name match
+                  case None        => newName
+                  case Some(value) => value.value
+
+              params += s"$formalName: ${scalaType(of)}"
+              arguments += formalName
+
+          val parametersList = params.result().mkString(", ")
+          val argumentsList = arguments.result().mkString(", ")
+
+          line(s"inline def apply(${parametersList}): $returnType = ") 
+          nest:
+            line(s"v.apply($argumentsList)")
+
       }
     end if
   }
