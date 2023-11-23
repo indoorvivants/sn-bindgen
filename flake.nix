094da00993297b2f08818ae9fa1f527c951c3686
Corey O'Connor <coreyoconnor@gmail.com>
2023-11-23 06:38:40 -0800

    add nix flake (#244)
    `nix develop` to get a developer environment. `nix shell` to build `bindgen` into profile.
    The `depsWarmupCommand` is a bit complicated: Has to compile enough but not too much.

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "sn-bindgen";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  inputs.sbt.url = "github:zaninime/sbt-derivation/master";
+  inputs.sbt.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.systems.url = "github:nix-systems/default";
+
+  outputs = {
+    self,
+    nixpkgs,
+    sbt,
+    systems
+  }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = eachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          default = sbt.mkSbtDerivation.${system} {
+            pname = "sn-bindgen";
+            version = "0.1.0";
+            src = self;
+            depsSha256 = "sha256-3sgPKN1/0lxSxOtetlWS/NobXIpQ6a7Ygk2GOprYIcg=";
+            buildPhase = ''
+              sbt 'show buildBinary'
+            '';
+            depsWarmupCommand = ''
+              sbt 'update'
+              sbt 'iface/compile ; iface2_12/compile ; iface3/compile'
+              sbt 'ifaceNative/compile ; ifaceNative2_12/compile ; ifaceNative3/compile'
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              cp bin/bindgen $out/bin/
+            '';
+            buildInputs = with pkgs; [
+              libclang
+            ];
+            nativeBuildInputs = with pkgs; [
+              clang
+              libunwind
+              stdenv
+              which
+              zlib
+            ];
+          };
+        }
+      );
+    };
+}
